f070c768337c631a8918acb05e78460fe9b0194a
Spike <spike@spikeboy.local>
2021-01-17 13:50:18 +0100

    A first pass implementing UCC structure - adding a log img - removing all css from initial template project - changing main.js to use a div inside body for the app

---
+++ src/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    import App from "./App.svelte";
+</script>
+
+<header>
+    <img src="/imgs/logo.png" alt="use case logo" />
+    <h1>Use Case Collector</h1>
+    <h2>Help us collect existing use cases and come up with new ones</h2>
+</header>
+
+<style>
+</style>
---
+++ src/UseCaseInput.svelte
@@ -0,0 +1,12 @@
+<script>
+</script>
+
+<main>
+    <p>As</p>
+    <p>I want to use</p>
+    <p>To</p>
+    <p>So that</p>
+</main>
+
+<style>
+</style>
