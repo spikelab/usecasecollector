71b7a4e3c6190ef77dc5025d4e22b9bcd145fe80
Spike <spike@spikeboy.local>
2021-01-17 20:26:13 +0100

    initial tailwind integration

--- src/App.svelte
+++ src/App.svelte
@@ -7,9 +7,7 @@
 <Header />
 <UseCaseInput />
 <UseCaseList />
-<svg
-    width="30px"
-    height="30px"
+<svg class="w-6 h-6"
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
     fill="currentColor"
@@ -19,3 +17,11 @@
         d="M13.03 8.22a.75.75 0 010 1.06l-4.25 4.25a.75.75 0 01-1.06 0L3.47 9.28a.75.75 0 011.06-1.06l2.97 2.97V3.75a.75.75 0 011.5 0v7.44l2.97-2.97a.75.75 0 011.06 0z"
     />
 </svg>
+
+<style global>
+    /* purgecss start ignore */
+    @import "tailwindcss/base";
+    @import "tailwindcss/components";
+    /* purgecss end ignore */
+    @import "tailwindcss/utilities";
+</style>
